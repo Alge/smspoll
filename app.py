685afb4fe098be7a2adfa82c6debbf9a4c631643
Martin Alge <martin@alge.se>
2019-04-16 10:18:58 +0200

    disabled xsrf verification

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
     "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
     "login_url": "/login",
-    "xsrf_cookies": True,
+    # "xsrf_cookies": True,
     "debug": True
 }
 
